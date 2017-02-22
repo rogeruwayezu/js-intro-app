12cad2ecc546624e9fb0f5697d36b180469637a6
rogeruwayezu <rogeruwayezu@gmail.com>
2017-02-22 18:25:13 +0200

    added new game for three boxes

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,1 +1,9 @@
-
+var count = 1
+function jackhammer(){
+  if (count === 5) {
+    alert("You win!");
+  } else {
+    console.log("keep looking");
+    count++;
+  }
+}
--- app/assets/javascripts/three.js
+++ app/assets/javascripts/three.js
@@ -1,1 +1,19 @@
+var boxClick_1 = 0
+var boxClick_2 = 0
+var boxClick_3 = 0
+function ClickAllTheBoxes(boxClick){
+  if (boxClick_1 > 0 && boxClick_2 > 0 && boxClick_3 > 0){
+    alert("you have won");
+  }else if (boxClick === boxClick_1){
+    console.log("keep trying");
+    boxClick_1++;
+  }else if (boxClick === boxClick_2){
+    console.log("keep trying");
+    boxClick_2++;
+  }else if (boxClick === boxClick_3){
+    console.log("keep trying");
+    boxClick_3++;
+  }
+
+  }
 
